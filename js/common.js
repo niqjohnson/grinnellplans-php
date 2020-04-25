3ab120b3a1872e42fbb24d916de27be93694c497
Nicholas Johnson <niq.johnson@gmail.com>
2020-04-25 14:12:32 -0500

    Add mobile menu

--- js/common.js
+++ js/common.js
@@ -17,3 +17,29 @@ function toggleShowHide(target, caller, showText, hideText) {
 	}
 	caller.innerHTML = linkText;
 }
+
+let mobileNav = {
+	init: function() {
+		let mobileNavTrigger = document.querySelectorAll('#mobile_nav_trigger')[0];
+		let body = document.querySelectorAll('body')[0];
+		body.dataset.menuVisible = this.isVisible;
+		mobileNavTrigger.addEventListener('click', (event) => {
+			if (this.isVisible === true) {
+				window.scrollTo(0, parseInt(body.style.top || '0') * -1);
+				body.style.position = '';
+				body.style.top = '';
+				this.isVisible = false;
+			} else {
+				body.style.position = 'fixed';
+				body.style.top = `-${window.scrollY}px`;
+				this.isVisible = true;
+			}
+			body.dataset.menuVisible = this.isVisible;
+		});
+	},
+	isVisible: false
+}
+
+window.onload = (event) => {
+	mobileNav.init();
+};
--- views/templates/tableless/mainpanel.tpl.php
+++ views/templates/tableless/mainpanel.tpl.php
@@ -1,8 +1,13 @@
-<div id="nav"><div>
+<div id="nav">
+<button id="mobile_nav_trigger" type="button">
+    <span class="mobile_nav_trigger_text">Show navigation</span>
+</button>    
+<div>
 <?php
 $this->linkhome_template->display();
 $this->fingerbox_template->display();
 $this->links_template->display();
 $this->autoread_template->display();
 ?>
-</div></div>
+</div>
+</div>
